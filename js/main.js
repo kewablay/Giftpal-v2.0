4a94c4f2937e1627e1ae6e3d72910db6db9f6818
kewablay <kojokewajunior123@gmail.com>
2021-10-19 20:04:26 +0000

    done with first section of landing page

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// const navToggler = document.getElementsByClassName('nav-toggler')
+// const navMenu = document.getElementsByClassName('nav-item-list')
+
+
+// navToggler.addEventListener("click", () => {
+//     navToggler.classList.toggle('active');
+//     navMenu.classList.toggle('active');
+
+//     if(navMenu.classList.contains("active")){
+//         navMenu.style.maxHeight = navMenu.scrollHeight + "px";
+//     }
+//     else{
+//         navMenu.removeAttribute("style");
+//     }
+// });
