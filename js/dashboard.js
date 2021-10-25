34593221db224340f2ffecca903533f93fdee55c
kewablay <kojokewajunior123@gmail.com>
2021-10-25 11:44:40 +0000

    added js

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,25 +3,49 @@ var sidebarToggle = document.getElementById('sidebar-toggle');
 var nav = document.getElementById('nav-items');
 var navToggle = document.getElementById('nav-toggle');
 
+
+// document.onclick = function(c){
+//     if (c.target.id !== 'side-bar' && c.target.id !== 'sidebar-toggle'){
+//         sidebarToggle.classList.remove('active');
+//         sidebar.classList.remove('active');
+//     }
+// }
+
+
 // side bar toggle 
 sidebarToggle.onclick = function() {
     sidebarToggle.classList.toggle('active');
-}
-
-// side bar toggle 
-function showSidebar(){
-    sidebar.style.left= '0';
+    sidebar.classList.toggle('active');
 }
 
 function hideSidebar(){
-    sidebar.style.left= "-300px";
+    sidebarToggle.classList.remove('active');
+    sidebar.classList.remove('active');
 }
 
-$('body').on('click', function(){
-    if( parseInt(sidebar.css('left') ) > 0 ){
-      hideSidebar();
-    }
-  });
+
+// nav bar toggle
+
+navToggle.onclick = function() {
+    navToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+
+
+
+
+
+
+// // side bar toggle 
+
+
+
+// $('body').on('click', function(){
+//     if( parseInt(sidebar.css('left') ) > 0 ){
+//       hideSidebar();
+//     }
+//   });
 
 
 // navbar toggle 
