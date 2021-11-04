90f0b85b73c4f39b080d7ef6709b1a3ba07877ea
kewablay <kojokewajunior123@gmail.com>
2021-11-04 12:04:12 +0000

    modified

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ navToggle.onclick = function(){
     nav.classList.toggle('active');
 }
 
-document.addEventListener("click", () => {
+document.addEventListener("click", (e) => {
     if (nav.classList.contains("active")) {
       nav.classList.remove("active");
       navToggle.classList.remove("active");
